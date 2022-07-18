e39d7cca43c2ebbe4f00b5e4febb1b3258046bc1
X <1170531734@qq.com>
2022-07-18 23:03:16 +0800

    处理一些

--- LeeCoode/Program.cs
+++ LeeCoode/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿//二叉树的中序遍历
+
+using System;
+using System.Collections.Generic;
 
 namespace LeeCoode
 {
+    
+  public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            int GetTree(TreeNode root)
+            {
+                if (root.val==null)
+                {
+
+                }
+            }
+
         }
     }
 }
---
+++ 二叉树的中序遍历/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 二叉树的中序遍历
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}                                                                                                                                                                 
