c708d7a4cc4a1e715254af3589ce4d690dc3203a
Panda0000000000000 <1170531734@qq.com>
2022-07-21 18:39:01 +0800

    处理些算法

---
+++ Z字形变换/Z字形变换.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Z字形变换
+{
+    class Z字形变换
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string Convert(string s, int numRows)
+        {
+            char[] charArr = s.ToCharArray();
+            char[,] charArr_Resault = new char[charArr.Length, charArr.Length];
+
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                char a = charArr[i];
+
+                int Left = i22;
+            }
+        }
+    }
+}
---
+++ 二叉树的中序遍历/二叉树的中序遍历.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 二叉树的中序遍历
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class 二叉树的中序遍历
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //递归
+        public static IList<int> InorderTraversal_1_S(TreeNode root)
+        {
+            IList<int> resault = new List<int>();
+
+            inorder(root, ref resault);
+            return resault;
+
+            void inorder(TreeNode root, ref IList<int> resault)
+            {
+                if (root == null)
+                {
+                    return;
+                }
+
+                inorder(root.left, ref resault);
+                resault.Add(root.val);
+                inorder(root.right, ref resault);
+            }
+        }
+
+        //迭代
+        public static IList<int> InorderTraversal_2(TreeNode root)
+        {
+            IList<int> resault = new List<int>();
+            //迭代
+            Stack<TreeNode> stk = new Stack<TreeNode>();
+            while (root!=null || stk.Count>0)
+            {
+                while (root!=null)
+                {
+                    stk.Push(root);
+                    root = root.left;
+                }
+
+                root = stk.Pop();
+                resault.Add(root.val);
+                root = root.right;
+            }
+
+            return resault;
+        }
+
+        //Morris 
+        public static IList<int> InorderTraversal_3(TreeNode root)
+        {
+            IList<int> ans = new List<int>();
+            while (root!=null)
+            {
+                if (root.left == null)
+                {
+                    ans.Add(root.val);
+                    root = root.right;
+                }
+                else
+                {
+                    TreeNode pre = root.left;
+                    while (pre.right!=null && pre.right!=root)
+                    {
+                        pre = pre.right;
+                    }
+
+                    if (pre.right == null)
+                    {
+                        pre.right = root;
+                        root = root.left;
+                    }
+                    else
+                    {
+                        pre.right = null;
+                        ans.Add(root.val);
+                        root = root.right;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+}
