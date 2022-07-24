83d113ef02340341e2395f4bba9b85131f936e97
X <1170531734@qq.com>
2022-07-24 20:57:09 +0800

    处理二分查找的打印

--- 10猜数字大小/10猜数字大小.cs
+++ 10猜数字大小/10猜数字大小.cs
@@ -4,24 +4,38 @@ namespace _10猜数字大小
 {
     class _10猜数字大小
     {
+
+        static int _Resault = 1702766719;
+
         static void Main(string[] args)
         {
             Console.WriteLine(1/2);
 
+            double n = 2126753390;
+            double resault =  GuessNumber(2126753390);
+
             Console.ReadKey();
         }
 
-        public int GuessNumber(int n)
+        public static double GuessNumber(double n)
         {
-            int left = 1, right = n;
+            double left = 2, right = n;
+
+            double int_LoopCount = 0;
 
             while (left < right)
             {
-                // int middle = (left+right)/2;
-                int middle = ((right - left) / 2) + left;
-                int resault = guess(middle);
+                //int middle = (left + right) / 2;
+                double middle = ((right - left) / 2.0) + left;
+                double resault = guess(middle);
+
+                int_LoopCount++;
+
+                Console.WriteLine("执行的步骤 "+int_LoopCount+ " Left值：" + left+"  Right值："+right+" middle值："+middle);
+
                 if (resault == 0)
                 {
+                    Console.WriteLine("执行的总次数为：" + int_LoopCount);
                     left = middle;
                     return left;
                 }
@@ -35,6 +49,20 @@ namespace _10猜数字大小
                 }
             }
             return left;
+        }
+
+        static double guess(double n)
+        {
+            if (n== _Resault)
+            {
+                return 0;
+            }
+            if (n< _Resault)
+            {
+                return 1;
+            }
+
+            return -1;
         }
     }
 }
