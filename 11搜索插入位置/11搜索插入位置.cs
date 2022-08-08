0816ad12b1b2fc54e041d7f82fd8aad7e412cb46
Panda0000000000000 <1170531734@qq.com>
2022-08-08 21:05:08 +0800

    新增题库

--- 11搜索插入位置/11搜索插入位置.cs
+++ 11搜索插入位置/11搜索插入位置.cs
@@ -8,26 +8,23 @@ namespace _11搜索插入位置
         {
             Console.WriteLine("Hello World!");
 
-            int resault = SearchInsert(new int[] { 1, 3, 5, 6 }, 7);
+            int resault = SearchInsert(new int[]{ 1, 3, 5, 6 }, 2);
         }
 
+        //二分法的这个写法还是不太懂，但是不在这里浪费时间了
         public static int SearchInsert(int[] nums, int target)
         {
             int l = 0, r = nums.Length-1;
-            while (l<r)
+            while (l<=r)
             {
                 int m = (r - l) / 2 + l;
-                if (nums[l] > target)
-                {
-                    r = m - 1;
-                }
-                if (nums[l] < target)
+                if (nums[m] < target)
                 {
                     l = m + 1;
                 }
-                if (nums[l] == target)
+                else
                 {
-                    return l;
+                    r = m - 1;
                 }
             }
 
---
+++ 12山脉数组的峰顶索引/12山脉数组的峰顶索引.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _12山脉数组的峰顶索引
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        //1.暴力解法  太简单，写出来也是侮辱智商  就不写具体代码了
+
+        //2.二分解法
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            int n = arr.Length;
+            int l = 1, r = n - 2, ans = 0;
+            while (l<=r)
+            {
+                int mid = (r - l) >> 1 + l;
+                if (arr[mid] > arr[mid + 1])
+                {
+                    ans = mid;
+                    r= mid - 1;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
