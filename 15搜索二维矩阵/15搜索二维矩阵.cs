a278e3f46e0e2c1c6fcbe2fbd9aa4595a1ac1a53
X <1170531734@qq.com>
2022-08-11 00:52:15 +0800

    处理搜索二维矩阵

--- 15搜索二维矩阵/15搜索二维矩阵.cs
+++ 15搜索二维矩阵/15搜索二维矩阵.cs
@@ -7,14 +7,15 @@ namespace _15搜索二维矩阵
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[,] a = new int[1, 1] { { 1 } };
         }
 
         //从数组的右上角开始找
         public static bool SearchMatrix(int[][] matrix, int target)
         {
             int col = matrix[0].Length - 1;
-            int row = 0;
-            while (col>=0&&row>=0)
+            int row = 0;    //行 
+            while (col>=0&&row<matrix.Length)
             {
                 if (matrix[row][col]==target)
                 {
