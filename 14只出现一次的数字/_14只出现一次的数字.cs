2f3ca2d4d9c16345507737deed801be705787fa9
Panda0000000000000 <1170531734@qq.com>
2022-08-10 21:01:50 +0800

    搜素二维矩阵

--- 14只出现一次的数字/_14只出现一次的数字.cs
+++ 14只出现一次的数字/_14只出现一次的数字.cs
@@ -2,11 +2,13 @@
 
 namespace _14只出现一次的数字
 {
-    class Program
+    class _14只出现一次的数字
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        //使用异或
     }
 }
---
+++ 15搜索二维矩阵/15搜索二维矩阵.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _15搜索二维矩阵
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //从数组的右上角开始找
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+            int col = matrix[0].Length - 1;
+            int row = 0;
+            while (col>=0&&row>=0)
+            {
+                if (matrix[row][col]==target)
+                {
+                    return true;
+                }
+                if(matrix[row][col] > target)
+                {
+                    col--;
+                }
+                else
+                {
+                    row++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
