f4cddd9b966740c9976ce62963f7b47a57031fe6
Panda0000000000000 <1170531734@qq.com>
2022-08-11 21:04:14 +0800

    新增算法

---
+++ 16合并两个有序数组/16合并两个有序数组.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _16合并两个有序数组
+{
+    class _16合并两个有序数组
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
+        }
+
+        //归并排序
+        public static void Merge_1(int[] nums1, int m, int[] nums2, int n)
+        {
+            int a = 0, b = 0;
+            int[] resault=new int[m+n];
+            int resault_Pos=0;
+
+            int a_Length = nums1.Length;
+            int b_Length = nums2.Length;
+
+            while (a<m||b<n)
+            {
+                if (a>= m)
+                {
+                    resault[resault_Pos++] = nums2[b++];
+                    continue;
+                }
+                if (b>= n)
+                {
+                    resault[resault_Pos++] = nums1[a++];
+                    continue;
+                }
+                if (nums1[a] <= nums2[b])
+                {
+                    resault[resault_Pos++] = nums1[a++];
+                }
+                else
+                {
+                    resault[resault_Pos++] = nums2[b++];
+                }
+            }
+
+            for (int i = 0; i < resault.Length; i++)
+            {
+                nums1[i] = resault[i];
+            }
+        }
+
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+
+        }
+    }
+}
---
+++ 17鸡蛋掉落/17鸡蛋掉落.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _17鸡蛋掉落
+{
+    class _17鸡蛋掉落
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int SuperEggDrop(int k, int n)
+        {
+            int f = 0;
+            int x = n / 2;
+
+            if(f<=k)
+            {
+                if()
+                {
+
+                }
+            }
+        }
+    }
+}
