80b3852ed21c42764b24cc575f591baa75e14708
Summer <1170531734@qq.com>
2022-10-30 20:21:55 +0800

    新增快速排序的算法部分

---
+++ 73快速排序/73快速排序.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] a = new int[] { 9, 10, 7, 6, 8, 5, 19 };
+Sort_Fast(ref a);
+
+Console.ReadKey();
+static void Sort_Fast(ref int[] array)
+{
+
+    void Sort(ref int[] array,int l,int r)
+    {
+        if (l>=r)
+        {
+            return;
+        }
+
+        int point = l;
+        int point_Value = array[l];
+
+        while (l < r)
+        {
+            while(r>l && array[r] > point_Value)
+            {
+                r--;
+            }
+
+            array[l] = array[r];
+
+            if (l<r)
+            {
+                l++;
+            }  
+
+            while (l<r && array[l] < point_Value)
+            {
+                l++;
+            }
+            array[r] = array[l];
+            if (r>l)
+            {
+                r--;
+            }
+        }
+
+        array[l] = point_Value;
+
+        if (l==r&&l ==0)
+        {
+            return;
+        }
+
+        Sort(ref array, 0, l-1);
+        Sort(ref array, l,array.Length-1);
+    }
+
+    Sort(ref array, 0, array.Length-1);
+}   
